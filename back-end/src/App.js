6e76a028f83a0be90ad45613fb6ca19c1b95c07f
3c2h <Charles_Chiacchia_student2018@wilder.school>
2019-12-06 17:51:32 +0100

    added basic local auth

--- back-end/src/App.js
+++ back-end/src/App.js
@@ -2,14 +2,16 @@ import React, { Component } from "react";
 import { Admin, Resource } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
 import { UserList, UserEdit, UserCreate } from './users';
-
+import authProvider from "./authProvider";
+// TODO replace with JWT(?) for prod.
 const dataProvider =
   jsonServerProvider("https://jsonplaceholder.typicode.com");
+  //TODO interface with DB for prod.
 
 class App extends Component {
   render() {
     return (
-      <Admin dataProvider={dataProvider}>
+      <Admin dataProvider={dataProvider} authProvider={authProvider}>
         <Resource
           name="users"
           list={UserList}
